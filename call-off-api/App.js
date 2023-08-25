5184212d7632577bd947388eb9b319194b91104e
THO4211 <shannon.thouvenin@stateauto.com>
2023-08-25 16:01:39 -0400

    added new get function, data is stilnot returning, but no errors

--- call-off-api/App.js
+++ call-off-api/App.js
@@ -6,6 +6,7 @@ const app = express();
 const db = mongoose.connect("mongodb://127.0.0.1:27017/calloff");
 const port = process.env.PORT || 8080;
 const CallOff = require("./models/callOffModel");
+const callOffModel = require("./models/callOffModel");
 const callOffRouter = require("./routes/callOffRouter")(CallOff);
 
 app.use(cors());
@@ -13,8 +14,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/api", callOffRouter);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my Nodemon API!");
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my Nodemon API!");
+// });
+
+app.get("api/calloffs", async (req, res) => {
+  try {
+    const calloffs = await callOffModel.find();
+    res.json(calloffs);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.server = app.listen(port, () => {
